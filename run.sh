cacaca1c9e73c28bcae444c1e029b80252316b33
DevinKott <dak3386@rit.edu>
2020-05-15 17:36:11 -0400

    update run script

--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
-echo "===== Copying service file"
+echo "===== Removing old service file"
+sudo systemctl disable motion.service
+sudo rm -rf /etc/systemd/system/motion.service
+
+echo "===== Copying new service file"
 sudo cp motion.service /etc/systemd/system/
 
 echo "===== Reloading daemons"
 sudo systemctl daemon-reload
 
 echo "===== Starting service"
-sudo systemctl enable motion.service
 sudo systemctl start motion.service
+sudo systemctl enable motion.service
