e8437acd8e673f05430c5d2e6aec9d3948120516
DevinKott <dak3386@rit.edu>
2020-05-15 17:23:29 -0400

    update build script

--- build.sh
+++ build.sh
@@ -1,3 +1,18 @@
-sudo docker build -t devinkott/security:latest --no-cache .
-cat ./docker_password.txt | sudo docker login --username devinkott --password-stdin
-sudo docker push devinkott/security:latest
+echo "===== Building container"
+docker build -t devinkott/security:latest --no-cache .
+
+echo "===== Logging into docker"
+cat ./docker_password.txt | docker login --username devinkott --password-stdin
+
+echo "===== Pushing container"
+docker push devinkott/security:latest
+
+echo "===== Copying service file"
+sudo cp motion.service /etc/systemd/system/
+
+echo "===== Reloading daemons"
+sudo systemctl daemon-reload
+
+echo "===== Starting service"
+sudo systemctl start motion.service
+sudo systemctl enable motion.service
